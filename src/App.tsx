eff9c4aa66b16d8023460fa1874546b87fce5d2e
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-17 15:05:43 +0600

    ✅Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header/Header';
 
 function App() {
     return (
         <main>
-            <h1>Hello World</h1>
+            <Header />
         </main>
     );
 }
