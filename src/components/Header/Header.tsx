2b6052fd9bcca668a21e8fc5966375ec299fb356
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-17 15:35:05 +0600

    ❤Header Left fully done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,14 +7,14 @@ const Header = () => {
                 <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31" className="h-7" alt="Microsoft Logo" />
 
                 <nav>
-                    <ul className="flex text-sm ml-10">
-                        <li className="mx-2">Microsoft 365</li>
-                        <li className="mx-2">Office</li>
-                        <li className="mx-2">Windows</li>
-                        <li className="mx-2">Surface</li>
-                        <li className="mx-2">Xbox</li>
-                        <li className="mx-2">Deals</li>
-                        <li className="mx-2">Support</li>
+                    <ul className="flex text-sm ml-10 lg:container">
+                        <li className="mx-2 hover:border-black border-transparent border-b-2 hover:border-current">Microsoft 365</li>
+                        <li className="mx-2 hover:border-black border-transparent border-b-2 hover:border-current">Office</li>
+                        <li className="mx-2 hover:border-black border-transparent border-b-2 hover:border-current">Windows</li>
+                        <li className="mx-2 hover:border-black border-transparent border-b-2 hover:border-current">Surface</li>
+                        <li className="mx-2 hover:border-black border-transparent border-b-2 hover:border-current">Xbox</li>
+                        <li className="mx-2 hover:border-black border-transparent border-b-2 hover:border-current">Deals</li>
+                        <li className="mx-2 hover:border-black border-transparent border-b-2 hover:border-current">Support</li>
                     </ul>
                 </nav>
             </div>
