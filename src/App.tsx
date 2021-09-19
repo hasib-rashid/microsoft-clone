f1c877895fcc126cbb99b972aa60aff177b19cdc
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-19 12:12:07 +0600

    ✨Carousel Is Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Carousel from './components/Carousel/Carousel';
 import Header from './components/Header/Header';
 
 function App() {
     return (
         <main>
             <Header />
+            <Carousel />
         </main>
     );
 }
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,54 @@
+import Carousel from "react-multi-carousel";
+
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1,
+        paritialVisibilityGutter: 60
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        paritialVisibilityGutter: 50
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        paritialVisibilityGutter: 30
+    }
+};
+
+// Because this is an inframe, so the SSR mode doesn't not do well here.
+// It will work on real devices.
+const Simple = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <Carousel
+                swipeable={true}
+                draggable={true}
+                showDots={true}
+                responsive={responsive}
+                infinite={true}
+                autoPlaySpeed={5000}
+                autoPlay
+                keyBoardControl={true}
+                transitionDuration={500}
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                className="w-11/12"
+            >
+                <div className="w-full flex">
+                    <span className="carousel-span h-full w-2/4">Hello</span>
+                    <img className="w-2/4" src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4G1h6?ver=151f&q=0&m=8&h=303&w=539&b=%23FFFFFFFF&l=f&x=800&y=224&s=1320&d=742&aim=true" alt="ligon deez" />
+                </div>
+                <div>
+                    <span className="carousel-span"></span>
+                    <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3NR20?ver=7bd2&q=0&m=8&h=303&w=539&b=%23FFFFFFFF&l=f&x=861&y=302&s=1119&d=629&aim=true" alt="ligon deez" />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Simple
