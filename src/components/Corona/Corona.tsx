10c01ef5bca1ac32de0f382fce129f76930b2380
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-24 22:49:30 +0600

    🥱Corona Done (Im sleepy)

--- src/components/Corona/Corona.tsx
+++ src/components/Corona/Corona.tsx
@@ -3,14 +3,16 @@ import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 const Corona = () => {
     return (
-        <article>
-            <h1>Microsoft's response to COVID-19</h1>
-            <p>Read how we’re responding to the COVID-19 outbreak, and get resources to help.</p>
-            <button>
-                <p>Learn More</p>
-                <KeyboardArrowRightIcon />
-            </button>
-        </article>
+        <main className="flex flex-col justify-center items-center">
+            <article className="bg-gray-200 w-11/12 flex flex-col justify-center items-center mt-20 p-10">
+                <h1 className="text-3xl font-semibold">Microsoft's response to COVID-19</h1>
+                <p className="mt-4">Read how we’re responding to the COVID-19 outbreak, and get resources to help.</p>
+                <button className="flex mt-2 font-semibold">
+                    <p>Learn More</p>
+                    <KeyboardArrowRightIcon />
+                </button>
+            </article>
+        </main>
     )
 }
 
