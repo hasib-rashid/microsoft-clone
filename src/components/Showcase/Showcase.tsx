1da793811a3a80cf650d7386cdaaf2c4d3e4646f
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-24 15:04:48 +0600

    😶Buttons

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -6,6 +6,14 @@ const Showcase = () => {
             <section className="">
                 <h1>Microsoft 365</h1>
                 <p>Premium Office apps, extra cloud storage, advanced security, and more—all in one convenient subscription</p>
+                <div>
+                    <button>
+                        <p>For Up to 6 people</p>
+                    </button>
+                    <button>
+                        <p>For 1 person</p>
+                    </button>
+                </div>
             </section>
             <section className="">
                 <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4DRie?ver=3184&q=0&m=8&h=303&w=539&b=%23FFFFFFFF&l=f&x=1129&y=917&s=767&d=431&aim=true" alt="showcase pic" />
